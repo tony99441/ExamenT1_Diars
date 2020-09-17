54b783fa16985f1daa3537f7a4de999940bcfe7b
Anthony Briones Quiroz <n00017665@upn.pe>
2020-09-17 18:00:13 -0500

    CreacionExamen

---
+++ ExamenT1/ExamenT1/BD/T1Context.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamenT1.BD
+{
+    public class T1Context : DbContext
+    {
+       // public DbSet<Cuenta> Cuentas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=DESKTOP-BIGBBRB\\SQLEXPRESS;Database=ExamenT1;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+           // modelBuilder.ApplyConfiguration(new CuentaConfiguration());
+        }
+
+    }
+}
